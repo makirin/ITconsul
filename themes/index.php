d3ec9478dda96ad79316c875f91d4b752c6075d8
makirin <143609096+makirin@users.noreply.github.com>
2024-02-29 16:12:33 +0900

    Add files via upload

---
+++ themes/index.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html>
+
+<head>
+  <title>HairTech</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Hair Tech-「自然と調和する、自分らしい美しさ」をコンセプトに自然由来の原料を使ったヘアケアや、紙質・骨格に合わせて似合うスタイルを提案する美容室">
+  <meta name="viewport" content="Width=device-width,initial-scale=1.0">
+  <link rel="stylesheet" href="stlye.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400&family=Noto+Serif+JP&family=Zen+Old+Mincho&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  
+</body>
+
+</html>
